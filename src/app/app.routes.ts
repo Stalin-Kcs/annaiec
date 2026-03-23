6351eb5ba6eeb97e18aea3e0d812eb0347ea5549
stalin-kcs <kcsstalin@gmail.com>
2026-03-23 10:03:05 +0530

    Initial commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
-
-export const routes: Routes = [];
+import { Home } from './pages/home/home';
+import { About } from './pages/about/about';
+import { Services } from './pages/services/services';
+import { Achievements } from './pages/achievements/achievements';
+import { Contact } from './pages/contact/contact';
+export const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: Home },
+  { path: 'about', component: About },
+  { path: 'services', component: Services },
+  { path: 'achievements', component: Achievements },
+  { path: 'contact', component: Contact }
+];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,20 @@
-import { Component, signal } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import * as AOS from 'aos';
+import { Header } from './shared/header/header';
+import { Footer } from './shared/footer/footer';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Header, Footer],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {
-  protected readonly title = signal('annaiec.com');
+export class App implements AfterViewInit {
+    ngAfterViewInit() {
+    AOS.init({
+      duration: 1000,
+      once: true
+    });
+  }
 }
