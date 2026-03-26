86047d14d9bba314220e8035c0c418f9fcd2e9a6
Prabakar <prabakar@newgendigital.com>
2026-03-26 11:09:02 +0530

    Implement national recognition section on achievements page, enhance contact form validation, and add new styling and assets for about and services pages.

--- src/app/pages/services/services.ts
+++ src/app/pages/services/services.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-services',
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './services.html',
   styleUrl: './services.css',
 })
